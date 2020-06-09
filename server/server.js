ea360c7c4d367303b3c37016cc694825d1ba261c
Andrew Chart <andrewc@andertons.co.uk>
2020-06-09 12:33:08 +0100

    Redirect from 80 to 443 in production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: '../.env' });
 
-// Set up Express
+// Express and Server Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
@@ -11,21 +11,8 @@ const http = require('http');
 const https = require('https');
 
 const app = express();
-const port = (process.env.NODE_ENV === 'production' ? 443 : 3001);
 
-const apiErrors = require('./api/errors.js');
-
-
-// Set up a database connection
-const dbUrl = process.env.REACT_APP_MONGO_DB_URL;
-const dbOpts = { useUnifiedTopology: true };
-const dbName = process.env.REACT_APP_MONGO_DB_NAME;
-
-const Database = require('./database.js');
-var db = new Database(dbUrl, dbOpts, dbName);
-
-
-// Set up CORS
+// Express: Set up CORS
 const corsOrigins = process.env.REACT_APP_API_CORS_ORIGINS.split(","); //Array of origins
 
 const corsOpts = {
@@ -38,16 +25,25 @@ const corsOpts = {
   }
 }
 
-
-// Allow parsing of json request bodies
+// Express: Allow parsing of json request bodies
 app.use(bodyParser.json());
 
-// Enable GZIP compression
+// Express: Enable GZIP compression
 app.use(compression());
 
 
-// API ROUTES - These routes resolve any requests starting /api
-// Uses CORS settings
+// API: Set up a database connection
+const dbUrl = process.env.REACT_APP_MONGO_DB_URL;
+const dbOpts = { useUnifiedTopology: true };
+const dbName = process.env.REACT_APP_MONGO_DB_NAME;
+
+const Database = require('./database.js');
+var db = new Database(dbUrl, dbOpts, dbName);
+
+// API: Routes
+// These routes resolve any requests starting /api. Uses CORS settings.
+const apiErrors = require('./api/errors.js');
+
 app.use(
   '/api/:entity/:entityId?/:property?/:propertyId?',
   cors(corsOpts),
@@ -88,13 +84,14 @@ app.use(
   }
 );
 
-// API 404 - errors as JSON
+// API: 404 Errors
+// Delivers not found errors beneath /api as JSON instead of HTML
 app.get("/api/?*?", (req, res) => apiErrors.notFound(res));
 
 
-// MAIN APP ROUTES - Treat these routes as valid routes for a user to hit
-// directly. These will resolve to the index.html file and React Router will set
-// the correct app state.
+// App: Main App Routes
+// These are routes that are considered as valid routes for a user to hit directly.
+// They resolve to the index.html file and React Router will set the correct app state.
 const appUrls = [
   '/game/:gameId?',
   '/about',
@@ -105,15 +102,22 @@ app.get(appUrls, (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../build') });
 });
 
-// Use the build folder statically for files in the filesystem
+// App: Fileserver
+// Use the `build` folder statically to serve files from the filesystem
 app.use(express.static('../build'));
 
-// All other routes provide an html 404
+// App: 404 Errors
+// All other routes provide an HTML 404 page
 app.get("*", (req, res) => {
   res.sendFile('404.html', { root: path.join(__dirname, '../build') });
 });
 
-// Try to set up as Https with LetsEncrypt
+
+// Express: Server Listen
+// Determine port number depending on environment and start listening
+const port = (process.env.NODE_ENV === 'production' ? 443 : 3001);
+
+// Try to set up as an https server with LetsEncrypt
 let credentials;
 try {
   const domain = process.env.REACT_APP_DOMAIN;
@@ -130,11 +134,26 @@ try {
   if (port === 443) throw new Error(err);
 }
 
-// Set up a secure/insecure server
+// Set up the server object, with https if appropriate
 const server = (port === 443 ? https.createServer(credentials, app) :
                                http.createServer(app));
 
-// Start listening
+// Start listening on the preferred port
 server.listen(port, () => {
-  console.log(`HTTP Server listening on port ${port}`);
+  console.log(`Http server listening on port ${port}`);
 });
+
+// If a production server with https was set up, also allow redirects from port 80 to 443
+if(port === 443) {
+
+  const httpServer = express.createServer(app).listen(80);
+
+  httpServer.get('*', function(req, res) {
+      res.redirect('https://' + req.headers.host + req.url);
+  });
+
+  httpServer.listen(80, () => {
+    console.log(`Http to https redirect server listening on port 80`);
+  });
+
+}
