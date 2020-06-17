c29fe6cb4b0618dc34f9f03c10b1ed4239876313
Andrew Chart <andrewc@andertons.co.uk>
2020-06-17 23:21:10 +0100

    Testing Jenkins Github Webhook #3

--- src/ui_components/global/Header.js
+++ src/ui_components/global/Header.js
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1><Link to="/">Final Rendezvous</Link></h1>
+        <h1><Link to="/">Final Rendezvous2</Link></h1>
         <nav>
           <Link to="/">New Game</Link>
           <Link to="/how-to-play">How To Play</Link>
