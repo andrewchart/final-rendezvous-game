e3a1c57e04196a67721d0275dca1f8e7d5253385
Andrew Chart <andrewc@andertons.co.uk>
2020-06-18 22:03:29 +0100

    No sudo on pm2 restart

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo "Starting Final Rendezvous Jenkins Build Script"
 sudo git pull origin master
 sudo npm install --production
 sudo npm run build
-sudo pm2 restart all
+pm2 restart all
 echo "Final Rendezvous Build Script Finished"
 exit
