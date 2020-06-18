1be13e3cc4c5c9f81d3580fea9a86089d0ae8d35
Andrew Chart <andrewc@andertons.co.uk>
2020-06-18 19:59:59 +0100

    Prepared build script to be run with final rendezvous user

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Starting Final Rendezvous Jenkins Build Script"
-git pull origin master
-npm install --production
-npm run build
-pm2 restart all
+sudo git pull origin master
+sudo npm install --production
+sudo npm run build
+sudo pm2 restart all
 echo "Final Rendezvous Build Script Finished"
 exit
