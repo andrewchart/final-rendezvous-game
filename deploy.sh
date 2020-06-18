6f1d769d8e02c3113fbc8a53ae1289f5fef3e36f
Andrew Chart <andrewc@andertons.co.uk>
2020-06-18 19:19:52 +0100

    Add build script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Starting Final Rendezvous Jenkins Build Script"
+sudo git pull
+sudo npm install --production
+sudo npm run build
+sudo pm2 restart all
+echo "Final Rendezvous Build Script Finished"
+exit
