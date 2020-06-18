c293d6088bf71e5b08e375cfcd3fdf580827a058
Andrew Chart <andrewc@andertons.co.uk>
2020-06-18 22:20:57 +0100

    Remove pm2 restart in favour of --watch

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ echo "Starting Final Rendezvous Jenkins Build Script"
 sudo git pull origin master
 sudo npm install --production
 sudo npm run build
-pm2 restart all
 echo "Final Rendezvous Build Script Finished"
 exit
